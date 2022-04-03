ed05a272115b0a3709d403dd1bc8599017e496d6
Mukhamet1996 <50286432+Mukhamet1996@users.noreply.github.com>
2022-04-03 14:48:42 +0300

    Added max number

--- Collatz hypothesis.py
+++ Collatz hypothesis.py
@@ -2,12 +2,12 @@
 from time import sleep
 while True:
     x = input("Type anything: ")
-    n = 0
-    
+    n = 0 
+    maxnum = []
     try:
         x = int(x)
-        while x!=1:
-            sleep(0.5)
+        while x > 1:
+            sleep(0.1)
             n+=1
             if x % 2 == 0:
                 x/=2
@@ -15,6 +15,8 @@ while True:
             elif x % 2 !=0:
                 x = x*3 + 1
                 print(f'{n}. {x}')
+            maxnum.append(x)
+        print(f'Max: {max(maxnum)}')    
         print(f'Total: {n}')
     except ValueError:
         print('You entered uncorrect value')
